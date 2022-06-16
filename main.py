711937ba374b6a8aff96303699bc31381713f70e
Chintan <cap5192@gmail.com>
2022-06-15 21:45:47 -0500

    test

--- main.py
+++ main.py
@@ -1,34 +1,18 @@
-file = open("bgp.txt", 'r')
-data = file.readlines()
-new_data = []
-
+file = open("bgp.txt", "r")
+data = file.readlines()[17:]
+formatted_data = []
+neighbors = []
 for i in range(len(data)):
-    if i > 16:
-        new_data.append(data[i].split())
-for i in range(len(new_data)):
-    # ['10.100.1.0' '4''200']
-    each_neighbor = new_data[i]
-    for x in range(len(each_neighbor)):
+    formatted_data.append(data[i].split())
+for i in range(len(formatted_data)):
+    each_line = formatted_data[i]
+    # ['10.100.1.0', '4', '200', '26', '22', '199', '0', '0', '00:14:23', '23']
+    for x in range(len(each_line)):
         if x == 9:
-            state = each_neighbor[x]
-            if state.isdigit() == True:
-                if state > "0":
-                    print("done")
+            if each_line[9].isdigit() == True:
+                if int(each_line[9]) > 0:
+                    neighbors.append(each_line[0])
 
-
-# read_line = []
-# for i in range(len(data)):
-#     if i >= 17:
-#         new_data.append(data[i])
-# for i in new_data:
-#     x = i.split()
-#     read_line.append(x)
-#
-# for i in range(len(read_line)):
-#     entry = read_line[i]
-#     for x in range(10):
-#         if x == 9:
-#             if entry[9].isdigit() == True:
-#                 if entry[9] > "0":
-#                     print(f"Neighbor {entry[0]} is up!")
-
+print("Up neighbors are:")
+for i in neighbors:
+    print(i)
